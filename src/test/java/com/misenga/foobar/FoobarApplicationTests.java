81e427c17ceed59e4c32adf3772971ad8e643de7
Bruno Freire <brunofreire14@gmail.com>
2022-07-04 02:27:29 +0100

    first commit

---
+++ src/test/java/com/misenga/foobar/FoobarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.misenga.foobar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoobarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
